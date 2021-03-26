9099a361325422c12658a6c8902daaf57249726b
IgnacioNMiranda <i.miranda.f99@gmail.com>
2021-03-26 20:05:58 -0300

    feat: addition of dist folder to repo, version 1.1.1

---
+++ dist/commands/misc/resources/groupsDescriptions.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.groupsDescriptions = void 0;
+exports.groupsDescriptions = {
+    games: ':game_die: GAMES :game_die:',
+    misc: ':tickets: MISCELLANEOUS :tickets:',
+    music: '',
+};
+//# sourceMappingURL=groupsDescriptions.js.map
---
+++ dist/commands/misc/shameonyou.js
@@ -0,0 +1,56 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_commando_1 = require("discord.js-commando");
+const discord_js_1 = require("discord.js");
+const links_1 = require("./resources/links");
+const configuration_1 = __importDefault(require("../../config/configuration"));
+class ShameOnYouCommand extends discord_js_commando_1.Command {
+    constructor(client) {
+        super(client, {
+            name: 'shameonyou',
+            aliases: ['soy'],
+            group: 'misc',
+            memberName: 'shameonyou',
+            description: 'Disrespects some @User.',
+            args: [
+                {
+                    key: 'disrespectedPerson',
+                    prompt: 'Who do you want to disrespect?',
+                    type: 'user',
+                },
+            ],
+        });
+    }
+    run(message, args) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const gifs = links_1.links[1].gifs;
+            const randIndex = Math.floor(Math.random() * gifs.length);
+            const embedMessage = new discord_js_1.MessageEmbed()
+                .setDescription(`Shame on you! ${args.disrespectedPerson.username} !!`)
+                .setColor(configuration_1.default.embedMessageColor)
+                .setImage(gifs[randIndex]);
+            try {
+                yield message.say(embedMessage);
+                yield message.delete();
+            }
+            catch (error) {
+                console.log(error);
+            }
+            return;
+        });
+    }
+}
+exports.default = ShameOnYouCommand;
+//# sourceMappingURL=shameonyou.js.map
