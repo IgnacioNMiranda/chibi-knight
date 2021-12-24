e354f9d13f2e6cd85a2b9aa5e7b4c7c6471bde9e
IgnacioNMiranda <i.miranda.f99@gmail.com>
2021-12-24 00:46:21 -0300

    chore: delete dist folder from repo

--- dist/utils/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleUtil = void 0;
-const role_utils_1 = __importDefault(require("./role.utils"));
-exports.RoleUtil = role_utils_1.default;
-//# sourceMappingURL=index.js.map
